db5379ca8b0de764cfbaea8192f6a1ecbc89e69f
bluss <bluss@users.noreply.github.com>
2017-11-10 20:12:42 +0100

    Modify fuzz target 1 to allow longer patterns

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -3,11 +3,12 @@
 extern crate galil_seiferas;
 
 fuzz_target!(|data: &[u8]| {
-    if data.len() > 1 {
-        // use first byte as split point
-        let split_len = (data[0] as usize).min(data.len() - 1);
-        let data = &data[1..];
-        let (needle, hay) = data.split_at(split_len);
+    if data.len() > 2 {
+        // use first 2 bytes as split point
+        let (a, b) = (data[0] as usize, data[1] as usize);
+        let (_, data) = data.split_at(2);
+        let needle_split = ((a << 8) | b).min(data.len());
+        let (needle, hay) = data.split_at(needle_split);
         assert_eq!(galil_seiferas::util::brute_force_fast(hay, needle), galil_seiferas::gs_find(hay, needle));
     }
 });
