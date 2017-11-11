31eefe3a33933431be8eb4f1e84601c0b4aec28c
bluss <bluss@users.noreply.github.com>
2017-11-11 19:13:55 +0100

    Fuzz: Remove unused fuzz targets (the newer ones are better)

--- fuzz/fuzz_targets/fuzz_target_2.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate galil_seiferas;
-
-fuzz_target!(|data: &[u8]| {
-    let hay = data;
-    let needle = b"aaabaaabaaabaaabbbbb";
-    assert_eq!(galil_seiferas::util::brute_force_fast(hay, needle), galil_seiferas::gs_find(hay, needle));
-});
