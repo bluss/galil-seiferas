f2bfcb0852147cc662053e85d7d4d852c4037d23
bluss <bluss@users.noreply.github.com>
2017-11-20 18:34:19 +0100

    FEAT: Tweak the way scope_l,r are computed
    Just tweak the way the scope and has_scope are computed. Permuting this
    a bit improves the more realistic benchmarks significantly, for some reason.
    
    ```
     name                               zold_m ns/iter     zno_m ns/iter      diff ns/iter   diff %
     benches::bench_gs_find_itself1     19 (315 MB/s)      19 (315 MB/s)                 0    0.00%
     benches::bench_gs_find_itself2     38 (394 MB/s)      29 (517 MB/s)                -9  -23.68%
     benches::bench_gs_find_itself3     1,898 (435 MB/s)   1,027 (805 MB/s)           -871  -45.89%
     benches::bench_gs_find_itself4     1,854 (446 MB/s)   1,021 (809 MB/s)           -833  -44.93%
     benches::bench_gs_find_itself5     2,080 (407 MB/s)   1,182 (716 MB/s)           -898  -43.17%
     benches::bench_gs_periodic2_10     373 (536 MB/s)     335 (597 MB/s)              -38  -10.19%
     benches::bench_gs_periodic2_50     8,298 (602 MB/s)   8,085 (618 MB/s)           -213   -2.57%
     benches::bench_gs_periodic2_large  31,528 (634 MB/s)  31,122 (642 MB/s)          -406   -1.29%
     benches::bench_gs_periodic5_50     13,040 (958 MB/s)  12,759 (979 MB/s)          -281   -2.15%
     benches::bench_gs_strings_bad      14,747             14,149                     -598   -4.06%
     benches::bench_gs_strings_good     871                737                        -134  -15.38%
    ```

--- src/lib.rs
+++ src/lib.rs
@@ -504,33 +504,26 @@ fn search_simple<T: Eq>(text: &[T], pattern: &[T],
     let n = text.len();
     let m = pattern.len();
 
-    let (has_scope, scope_l, scope_r) = if let Some(hrp1) = *hrp1 {
-        // Scope of the k-HRP1 is [L, R]
-        // where
-        //  L = |v²| = 2 × period
-        //  R = z = length of prefix
-        //
-        // See Lemma 2 in [CR]:
-        //
-        // Any nonempty prefix u of x satisfies
-        //
-        // per(u) = Li / 2 if |u| is in [Li, Ri] for some i
-        // per(u) > |u| / k if not
-        //
-        let scope_l = hrp1.period * 2;
-        let scope_r = hrp1.len;
-        debug_assert!(scope_l <= scope_r);
-        (true, scope_l, scope_r)
-    } else {
-        (false, 0, 0)
-    };
-
+    // Scope of the k-HRP1 is [L, R]
+    // where
+    //  L = |v²| = 2 × period
+    //  R = |z| = length of periodic prefix
+    //
+    // See Lemma 2 in [CR]:
+    //
+    // Any nonempty prefix u of x satisfies
+    //
+    // per(u) = L / 2 if |u| is in [L, R]
+    // per(u) > |u| / k if not
+    //
+    let (scope_l, scope_r) = hrp1.map(|h| (h.period * 2, h.len))
+                                 .unwrap_or((!0, !0));
     let mut pos = *start_pos; // text position
     let mut j = *start_j;     // pattern position
     while pos <= n - m {
         j = longest_common_prefix_from(j, get!(text, pos..), pattern);
         let has_match = if j == m { Some(pos) } else { None };
-        if has_scope && j >= scope_l && j <= scope_r {
+        if j >= scope_l && j <= scope_r {
             pos += scope_l / 2;
             j -= scope_l / 2;
         } else {
