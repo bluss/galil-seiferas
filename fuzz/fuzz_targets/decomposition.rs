194fb38c9dd25164080bcca905c3c9e7307bc59e
bluss <bluss@users.noreply.github.com>
2017-11-23 23:02:20 +0100

    TEST: Use a strange equality function for decompostion fuzzing

--- fuzz/fuzz_targets/decomposition.rs
+++ fuzz/fuzz_targets/decomposition.rs
@@ -3,5 +3,5 @@
 extern crate galil_seiferas;
 
 fuzz_target!(|data: &[u8]| {
-    galil_seiferas::gs_find(data, data);
+    galil_seiferas::gs_find_by(data, data, |a, b| (a & 0xf) == (b & 0xf));
 });
